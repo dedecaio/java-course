d33ec46ab905b35f62db1b44eb4b4138fa503729
Caio <caio.florees2005@gmail.com>
2023-05-26 18:20:34 -0300

    Fazendo o problema 4

--- POO/problema1/Main/src/Main.java
+++ POO/problema1/Main/src/Main.java
@@ -7,24 +7,32 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        Product[] produto = new Product[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Qual o nome do produto que desejas adicionar?");
+            String name = sc.nextLine();
+            System.out.println("Quanto está custando:");
+            double price = sc.nextDouble();
+            System.out.println("Qual a quantidade que desejas adicionar?");
+            int quantity = sc.nextInt();
 
-        System.out.println("Qual o nome do produto que desejas adicionar?");
-        String name = sc.nextLine();
-        System.out.println("Quanto está custando:");
-        double price = sc.nextDouble();
-        System.out.println("Qual a quantidade que desejas adicionar?");
-        int quantity = sc.nextInt();
+            produto[i] = new Product(name, price, quantity);
 
-        Product produto = new Product(name,price,quantity);
+            System.out.println("\n"+produto[i]);
+
+            System.out.println("Quanto gostaria de remover da quantidade");
+            int unidadeRemovida = sc.nextInt();
+            produto[i].removeProducts(unidadeRemovida);
+            sc.nextLine();
+            System.out.println("\n"+produto[i]+ " agora");
+        }
+        for (int v = 0; v < n; v++){
+            System.out.println(produto[v]);
+        }
 
 
-        System.out.println("\n"+produto);
-
-        System.out.println("Quanto gostaria de remover da quantidade");
-        int unidadeRemovida = sc.nextInt();
-        produto.removeProducts(unidadeRemovida);
-
-        System.out.println("\n"+produto+ "agora");
 
 
         sc.close();
